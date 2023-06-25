699d499518e65884ca2b2812ff2929e3709666bc
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-25 16:44:27 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,29 +1,55 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import re
+
+def scrape_usajobs():
+    # Send a GET request to the USAJOBS website
+    response = requests.get("https://www.usajobs.gov/Search/Results")
+
+    if response.status_code == 200:
+        # Parse the HTML content using Beautiful Soup
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        # Find all job listings on the page
+        job_listings = soup.find_all("div", class_="job-listing")
+
+        # Create empty lists to store job data
+        titles = []
+        organizations = []
+        locations = []
+
+        for listing in job_listings:
+            # Extract job title
+            title = listing.find("h2", class_="position-title").text.strip()
+            titles.append(title)
+
+            # Extract organization name
+            organization = listing.find("span", class_="organization-name").text.strip()
+            organizations.append(organization)
+
+            # Extract job location
+            location = listing.find("span", class_="location").text.strip()
+            locations.append(location)
+
+        # Create a DataFrame to store the scraped data
+        data = pd.DataFrame({
+            "Title": titles,
+            "Organization": organizations,
+            "Location": locations
+        })
+
+        # Perform data cleaning and pre-processing
+        data["Location"] = data["Location"].str.replace(r"[\(\[].*?[\)\]]", "").str.strip()
+
+        # Save the data to a CSV file
+        data.to_csv("usajobs_data.csv", index=False)
+
+        print("Data scraped and saved successfully.")
+
+    else:
+        print("Error: Failed to retrieve data from USAJOBS.")
 
 
-def scrape_job_descriptions(url):
-    # Send a get request to the website
-    response = requests.get(url)
-
-    # Ensure that the request was successful
-    if response.status_code != 200:
-        return 'Request failed with status code {}'.format(response.status_code)
-
-    # Create a BeautifulSoup object and specify the parser
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    # Find job descriptions. This might change depending on the structure of the website.
-    job_descriptions = soup.find_all('div', {'class': 'jobDescription'})
-
-    # Create a list to store all the job descriptions
-    descriptions = []
-
-    for job in job_descriptions:
-        descriptions.append(job.text)
-
-    return descriptions
-
-
-url = 'https://www.indeed.com/'  # replace with the URL of the website you want to scrape
-descriptions = scrape_job_descriptions(url)
+# Call the function to initiate the scraping process
+scrape_usajobs()
