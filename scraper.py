bf17417714af66cd338d8e12b737847f38f99935
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-25 17:54:01 -0400

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ import os
 import requests
 import torch
 from bs4 import BeautifulSoup
+import json
 import pandas as pd
 import re
 import spacy
@@ -13,63 +14,62 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+import requests
+import pandas as pd
+import json
+
 
 def scrape_usajobs():
-    # Send a GET request to the USAJOBS website
-    response = requests.get("https://www.usajobs.gov/Search/Results")
+    host = 'data.usajobs.gov'
+    userAgent = 'your@email.address'  # replace with your email address
+    authKey = 'YourAPIKey'  # replace with your API key
+
+    url = 'https://data.usajobs.gov/api/search'
+    headers = {
+        "Host": host,
+        "User-Agent": userAgent,
+        "Authorization-Key": authKey,
+    }
+    params = {
+        'JobCategoryCode': '2210',  # this is for Information Technology Management jobs
+        'Keyword': 'Software Development',
+        'LocationName': 'Washington, DC',
+    }
+
+    response = requests.get(url, headers=headers, params=params)
 
     if response.status_code == 200:
-        # Parse the HTML content using Beautiful Soup
-        soup = BeautifulSoup(response.content, "html.parser")
+        data = response.json()
 
-        # Find all job listings on the page
-        job_listings = soup.find_all("div", class_="job-listing")
-        print("Number of job listings:", len(job_listings))
-        # Create empty lists to store job data
-        titles = []
-        organizations = []
-        locations = []
-        descriptions = []
+        titles = [item['MatchedObjectDescriptor']['PositionTitle'] for item in
+                  data['SearchResult']['SearchResultItems']]
+        organizations = [item['MatchedObjectDescriptor']['OrganizationName'] for item in
+                         data['SearchResult']['SearchResultItems']]
+        locations = [item['MatchedObjectDescriptor']['PositionLocation'][0]['LocationName'] for item in
+                     data['SearchResult']['SearchResultItems']]
+        descriptions = [item['MatchedObjectDescriptor']['QualificationSummary'] for item in
+                        data['SearchResult']['SearchResultItems']]
 
-        for listing in job_listings:
-            # Extract job title
-            title = listing.find("h2", class_="position-title").text.strip()
-            titles.append(title)
-
-            # Extract organization name
-            organization = listing.find("span", class_="organization-name").text.strip()
-            organizations.append(organization)
-
-            # Extract job location
-            location = listing.find("span", class_="location").text.strip()
-            locations.append(location)
-
-            # Extract job description
-            description = listing.find("div", class_="job-description").text.strip()
-            descriptions.append(description)
-
-        # Create a DataFrame to store the scraped data
-        data = pd.DataFrame({
-            "Title": titles,
-            "Organization": organizations,
-            "Location": locations,
-            "Description": descriptions
+        job_data = pd.DataFrame({
+            'Title': titles,
+            'Organization': organizations,
+            'Location': locations,
+            'Description': descriptions,
         })
 
         # Perform data cleaning and pre-processing
-        data["Location"] = data["Location"].astype(str).str.replace(r"[\(\[].*?[\)\]]", "").str.strip()
+        job_data["Location"] = job_data["Location"].astype(str).str.replace(r"[\(\[].*?[\)\]]", "").str.strip()
 
         # Save the data to a CSV file
-        data.to_csv("usajobs_data.csv", index=False)
+        job_data.to_csv('usajobs_data.csv', index=False)
+
         # Check the data
-        print("Number of job descriptions:", len(data["Description"]))
-        print("Number of non-empty job descriptions:", len(data[data["Description"].str.strip() != ""]))
+        print("Number of job descriptions:", len(job_data["Description"]))
+        print("Number of non-empty job descriptions:", len(job_data[job_data["Description"].str.strip() != ""]))
         print("First few rows of data:")
-        print(data.head())
+        print(job_data.head())
         print("Data scraped and saved successfully.")
-
     else:
-
         print("Error: Failed to retrieve data from USAJOBS.")
 
 
@@ -247,4 +247,4 @@ if __name__ == "__main__":
     scrape_usajobs()
     extract_features()
     train_resume_model()
-    app.run(debug=True)
+    #app.run(debug=True)
